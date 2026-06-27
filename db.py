ba27e3a1aea2eb4457bbc07d8d482f38c0807f34
AKSHITA VERMA <vakshita2004@gmail.com>
2026-06-27 10:23:44 +0530

    moved database credentials to environment variables

--- db.py
+++ db.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-DATABASE_URL = "mysql+pymysql://2UxfjkRq6GoxkSZ.root:n5IlzGcaBOCG6Zzi@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/test"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(
